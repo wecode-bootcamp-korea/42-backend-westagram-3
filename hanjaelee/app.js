b7feb20de0f249706b00aba9cb65c66f56524087
Hanjae Lee <jayhanjaelee@gmail.com>
2023-01-18 17:53:13 +0900

    [이한재] Assignment1: Express API - 초기 환경 세팅

--- hanjaelee/app.js
+++ hanjaelee/app.js
@@ -1,30 +1,46 @@
-const http = require('http')
+// Built-in packages
+
+// 3rd-party packages
+
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+
+// custom packages
+
+const { DataSource } = require('typeorm')
+
 const dotenv = require('dotenv')
+dotenv.config()
+
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized.')
+  })
 
 app = express()
 
-app.use(express.json())
-// app.use(cors())
+// Middleware : request response 사이에 middleware 를 거친다.  app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 
-dotenv.config()
-
-// app.get('/ping', (req, res) => {
-//   res.json({ message: 'pong' })
-// })
-
-app.get('/ping', cors(), (req, res) => {
+app.get('/ping', (req, res) => {
   res.json({ message: 'pong' })
 })
 
-const server = http.createServer(app)
 const PORT = process.env.PORT;
-
 const start = async () => {
-  server.listen(PORT, () => console.log(`server is listening to on ${PORT}`))
+  app.listen(PORT, () => console.log(`server is listening to on ${PORT}`))
 }
 
 start()
