abf56b8a96f532a7886712ab4cbe16a565c3af9c
wonkyung Choi <wonkyungchoi@wonkyungui-MacBookPro.local>
2023-01-20 10:58:35 +0900

    westagram backend project-Express 초기환경세팅 수정요청사항 이행

--- wonkyung/app.js
+++ wonkyung/app.js
@@ -1,14 +1,14 @@
-const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
 dotenv.config();
+require("dotenv").config();
 
 const { DataSource } = require('typeorm')
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -17,7 +17,7 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
+appDataSource.initialize()
   .then(() => {
       console.log("Data Source has been initialized!")
   });
@@ -34,11 +34,10 @@ app.get("/ping", (req,res) => {
   res.json({message:"pong"});
 });
 
-const server = http.createServer(app)
 const PORT =  process.env.PORT;
 
 const start = async () =>{
-   server.listen(PORT, () => console.log(`server is listening on ${PORT}` ));
+   app.listen(PORT, () => console.log(`server is listening on ${PORT}` ));
 }
 
 start()
