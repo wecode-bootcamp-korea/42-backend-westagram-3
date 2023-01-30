e9fe6d2596f09fbe55b1d90fc1c124be26a193bc
Hanjae Lee <jayhanjaelee@gmail.com>
2023-01-30 15:54:37 +0900

    [42기 이한재] Assignment - 1 회원가입시 유저 패스워드 암호화 적용.

--- hanjaelee/controllers/userController.js
+++ hanjaelee/controllers/userController.js
@@ -4,6 +4,10 @@ const signup = async (req, res) => {
   try {
     const { name, email, password, profileImage } = req.body
 
+    if (!password) {
+      return res.status(400).json({ message: 'No Password.' })
+    }
+
     const isCreated = await userService.signup(
       name,
       email,
@@ -14,7 +18,7 @@ const signup = async (req, res) => {
       throw new Error('Failed to Create User')
     }
 
-    res.status(200).json({ message: 'createUser' })
+    return res.status(200).json({ message: 'userCreated' })
   } catch (err) {
     console.error(err)
     return res.status(err.statusCode || 500).json({ message: err.message })
--- hanjaelee/services/userService.js
+++ hanjaelee/services/userService.js
@@ -1,17 +1,24 @@
+const bcrypt = require('bcrypt')
+
 const userDao = require('../models/userDao.js')
 
 const signup = async (name, email, password, profileImage) => {
+  const hashedPassword = await makeHashedPassword(password, 12)
 
   const isCreated = await userDao.createUser(
     name,
     email,
-    password,
+    hashedPassword,
     profileImage
   )
 
   return isCreated
 }
 
+const makeHashedPassword = (password, saltRounds) => {
+  return bcrypt.hash(password, saltRounds)
+}
+
 module.exports = {
-  signup
+  signup,
 }
