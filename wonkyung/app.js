fb05482a37c739aeaa2d3d356f97deca31268643
wonkyung Choi <wonkyungchoi@wonkyungui-MacBookPro.local>
2023-02-03 17:04:48 +0900

    westagram backend project-Express 현진행상황까지 추가,

--- wonkyung/app.js
+++ wonkyung/app.js
@@ -5,9 +5,6 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
-
-
-
 const { DataSource } = require('typeorm')
 
 const appDataSource = new DataSource({
@@ -37,9 +34,54 @@ app.use(morgan(''));
 app.get("/ping", (req,res) => {
   res.json({message:"pong"});
 });
+app.post("/users/signup", async(req, res) => {
+  const { name, email, profileImage, password } = req.body
 
+  await appDataSource.query(
+      `INSERT INTO users(
+          name, 
+          email,
+          profile_image,
+          password
+      ) VALUES (?, ?, ?, ?);
+      `, [name, email, profileImage, password]
+  );
+  
 
+  res.status(201).json({ message : "userCreated"});
+});
 
+app.post("/posts/signup", async(req, res,) => {
+  const { title, content, imageUrl, userId } = req.body;
+
+  await appDataSource.query(
+      `INSERT INTO posts(
+          title,
+          content,
+          image_url,
+          user_id
+      ) VALUES ( ?, ?, ?, ? );
+      `, [title, content, imageUrl, userId]
+  );
+
+  res.status(201).json({ message : "postCreated"});
+})
+
+app.get("/posts/list", async(req, res,) => {
+  await appDataSource.query(
+      `SELECT 
+            users.id as userId, 
+            users.profile_image as userProfileImage, 
+            posts.id as postingId, 
+            posts.image_url as postingImageUrl, 
+             posts.content as postingContent 
+          FROM users 
+          INNER JOIN posts 
+          ON users.id = posts.user_id
+      `, (err, posts) => {
+          return res.status(200).json(posts);
+      });
+});
 
 
 const PORT =  process.env.PORT;
